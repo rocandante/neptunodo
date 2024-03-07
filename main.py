f53cd278dd7ea6d2696b6757ad112819e884f75d
Juan Cardona <rocandante@gmail.com>
2024-03-07 15:59:20 -0500

    Se definen algunas propiedades para Page y se establece el tema por defecto

--- main.py
+++ main.py
@@ -2,7 +2,21 @@ import flet as ft
 
 
 def main(page: ft.Page):
-    page.add(ft.SafeArea(ft.Text("Hello, Flet!")))
+    page.title = "Neptunodo App"
+    page.window_maximizable = False
+    page.horizontal_alignment = ft.MainAxisAlignment.CENTER
+    page.window_resizable = False
+    page.window_width = "390"
+
+    # Set theme
+    page.theme_mode = ft.ThemeMode.LIGHT
+    theme = ft.Theme()
+    page.theme = theme
 
 
-ft.app(main)
+
+    page.update()
+
+
+# ft.app(target=main, view=ft.AppView.WEB_BROWSER)
+ft.app(target=main, assets_dir="assets")
