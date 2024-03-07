23aa8f061524eb33432046f38a6be72a62ab7009
Juan Cardona <rocandante@gmail.com>
2024-03-07 17:27:20 -0500

    Se establece el layout de Page en la clase MainPage

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+"""
+Neptunodo is a simple ToDo app in Python + Flet
+"""
+
 import flet as ft
 
 # Define los estilos para el tema light/dark
@@ -5,6 +9,41 @@ _dark: str = ft.colors.with_opacity(0.5, "white")
 _light: str = ft.colors.with_opacity(1, "black")
 
 toggle_style_sheet: dict = {"icon": ft.icons.DARK_MODE_ROUNDED, "icon_size": 18}
+
+
+class MainPage(ft.SafeArea):
+    def __init__(self, page: ft.Page):
+        super().__init__(minimum=10, maintain_bottom_view_padding=True)
+        self.page = page
+
+        # Define el encabezado
+        self.title: ft.Text = ft.Text("Neptunodo", size=20, weight="w800")
+        self.toggle: ft.IconButton = ft.IconButton(
+            **toggle_style_sheet, on_click=lambda e: self.switch(e)
+        )
+
+        self.main: ft.Column = ft.Column(
+            controls=[
+                ft.Row(
+                    alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
+                    controls=[self.title, self.toggle],
+                ),
+                ft.Divider(height=20),
+            ]
+        )
+
+        self.content = self.main
+
+    def switch(self, e):
+        if self.page.theme_mode == ft.ThemeMode.DARK:
+            self.page.theme_mode = ft.ThemeMode.LIGHT
+            self.toggle.icon = ft.icons.DARK_MODE_ROUNDED
+
+        else:
+            self.page.theme_mode = ft.ThemeMode.DARK
+            self.toggle.icon = ft.icons.LIGHT_MODE_ROUNDED
+
+        self.page.update()
 
 
 def main(page: ft.Page):
@@ -19,32 +58,7 @@ def main(page: ft.Page):
     theme = ft.Theme()
     page.theme = theme
 
-    # Define el encabezado
-    title: ft.Text = ft.Text("Neptunodo", size=20, weight="w800")
-    toggle: ft.IconButton = ft.IconButton(
-        **toggle_style_sheet, on_click=lambda e: switch(e)
-    )
-
-    mainpage: ft.Column = ft.Column(
-        controls=[
-            ft.Row(
-                alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
-                controls=[title, toggle],
-            ),
-            ft.Divider(height=20),
-        ]
-    )
-
-    def switch(e):
-        if page.theme_mode == ft.ThemeMode.DARK:
-            page.theme_mode = ft.ThemeMode.LIGHT
-            toggle.icon = ft.icons.DARK_MODE_ROUNDED
-
-        else:
-            page.theme_mode = ft.ThemeMode.DARK
-            toggle.icon = ft.icons.LIGHT_MODE_ROUNDED
-
-        page.update()
+    mainpage: object = MainPage(page)
 
     page.add(mainpage)
     page.window_width = "390"
